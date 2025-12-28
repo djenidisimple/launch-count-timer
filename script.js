615ffa54f78dd70d3d5929d318339044e8982326
djenidisimple <djenidigauss@gmail.com>
2025-12-28 14:04:32 +0300

    i add timer for new years

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
+let dateNewYears = new Date(`${parseInt(new Date().getFullYear()) + 1}-01-01 00:00:00`);
+let dateCurrent = new Date();
+let diff = dateNewYears - dateCurrent;
+let remainingDays = Math.floor(diff / (1000 * 60 * 60 * 24));
+let remainingHours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+let remainingMinutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+let remainingSeconds = Math.floor((diff % (1000 * 60)) / 1000);
+const formated = (value) => {
+    return parseInt(value) <= 9 ? "0" + value : value;
+}
 let days = document.getElementById("days");
 let hours = document.getElementById("hours");
 let minutes = document.getElementById("minutes");
 let seconds = document.getElementById("seconds");
 let s = 0, m = 0, h = 0, d = 0;
+days.innerText = formated(remainingDays);
+hours.innerText = formated(remainingHours);
+minutes.innerText = formated(remainingMinutes);
+seconds.innerText = formated(remainingSeconds);
+
 s = parseInt(seconds.textContent);
 m = parseInt(minutes.textContent);
 h = parseInt(hours.textContent);
@@ -12,8 +27,8 @@ setInterval(()=> {
     m = (s == 0) ?  (m <= 58) ? m + 1 : 0 : m;
     h = (m == 0 && s == 0) ? (h <= 58) ? h + 1 : 0 : h;
     d = (h == 0 && m == 0 && s == 0) ? (d <= 365) ? d + 1 : 0 : d;
-    seconds.innerText = (s <= 9) ? "0" + s : s;
-    minutes.innerText = (m <= 9) ? "0" + m : m;
-    hours.innerText =  (h <= 9) ? "0" + h : h;
-    days.innerText = (d <= 9) ? "0" + d : d;
+    seconds.innerText = formated(s);
+    minutes.innerText = formated(m);
+    hours.innerText =  formated(h);
+    days.innerText = formated(d);
 }, 1000);
